7ab80d41dbed8eb6c46787f156b84843c3641faa
Royce Schultz <rosc1351@colorado.edu>
2021-03-23 15:03:09 -0600

    Style changes. Breaks up functions into unit cases

--- PyTasks/src/main.py
+++ PyTasks/src/main.py
@@ -8,6 +8,10 @@ from influxdb_client import InfluxDBClient, Point, WritePrecision
 from influxdb_client.client.write_api import SYNCHRONOUS
 from slack_sdk.webhook import WebhookClient
 
+##
+## Config
+##
+
 # Initialize Influx Client
 token = os.environ['TOKEN']
 org = os.environ['ORG']
@@ -28,26 +32,39 @@ params = f'zip={zip_code}&appid={key}&units=metric'
 forecast_url = f'{open_weather_api_url}/data/2.5/forecast?{params}'
 current_weather_url = f'{open_weather_api_url}/data/2.5/weather?{params}'
 
-def ping_current_weather():
-    print('pinging weather api')
+
+##
+## Current Weather
+##
+
+def FetchCurrentWeather():
     res = requests.get(current_weather_url)
+    if not res.status_code == 200:
+        print(res.status_code)
+        print(res.content)
     assert res.status_code == 200
-    data = res.json()
-    dt = datetime.fromtimestamp(data['dt']).astimezone(pytz.utc)
-    print(dt)
-    write = { # InfluxDB is picky about types
-        'open_weather_temp': float(data['main']['temp']),
-        'open_weather_humidity': int(data['main']['humidity']),
-        'open_weather_wind_speed': float(data['wind']['speed']),
-        'open_weather_cloud_cover': int(data['clouds']['all']),
-        'open_weather_daylight_hours': float((data['sys']['sunset'] - data['sys']['sunrise']) / (60 * 60)),
+    return res.json()
+
+def ParseCurrentWeather(current_weather):
+    return { # InfluxDB is picky about types
+        'open_weather_temp': float(current_weather['main']['temp']),
+        'open_weather_humidity': int(current_weather['main']['humidity']),
+        'open_weather_wind_speed': float(current_weather['wind']['speed']),
+        'open_weather_cloud_cover': int(current_weather['clouds']['all']),
+        'open_weather_daylight_hours': float((current_weather['sys']['sunset'] - current_weather['sys']['sunrise']) / (60 * 60)),
     }
-    for key in write:
-        point = Point(key).tag("host", hostname).field("value", write[key]).time(dt, WritePrecision.S)
+
+def PingCurrentWeather():
+    print('pinging weather api')
+    current_weather = FetchCurrentWeather()
+    dt = datetime.fromtimestamp(current_weather['dt']).astimezone(pytz.utc)
+    print(dt)
+    for key, value in ParseCurrentWeather(current_weather).items():
+        point = Point(key).tag("host", hostname).field("value", value).time(dt, WritePrecision.S)
         write_api.write(bucket, org, point)
 
-ping_current_weather()
-schedule.every(5).minutes.do(ping_current_weather)
+PingCurrentWeather()
+schedule.every(5).minutes.do(PingCurrentWeather)
 
 
 def job(t):
