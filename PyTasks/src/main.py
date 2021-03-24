a1de618e096027907f0ecb8fc8a0f4acb7575252
Royce Schultz <rosc1351@colorado.edu>
2021-03-23 20:40:15 -0600

    Adds logging, catches errors from jobs to avoid resetting schedule on restart. Removes template job

--- PyTasks/src/main.py
+++ PyTasks/src/main.py
@@ -1,16 +1,29 @@
-import schedule
-import time
-import requests
 import os
+import sys
+import logging
+import time
 from datetime import datetime
 import pytz
+import requests
+import schedule
 from influxdb_client import InfluxDBClient, Point, WritePrecision
 from influxdb_client.client.write_api import SYNCHRONOUS
 from slack_sdk.webhook import WebhookClient
 
+
 ##
 ## Config
 ##
+
+# Logging
+root = logging.getLogger()
+root.setLevel(logging.DEBUG)
+
+handler = logging.StreamHandler(sys.stdout)
+handler.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s-%(levelname)s: %(message)s')
+handler.setFormatter(formatter)
+root.addHandler(handler)
 
 # Initialize Influx Client
 token = os.environ['TOKEN']
@@ -33,17 +46,27 @@ forecast_url = f'{open_weather_api_url}/data/2.5/forecast?{params}'
 current_weather_url = f'{open_weather_api_url}/data/2.5/weather?{params}'
 
 
+def RunFailSafe(job, *args, **kwargs):
+    '''Catch all errors to avoid resetting schedule'''
+    try:
+        job(*args, **kwargs)
+    except Exception as e:
+        logging.exception(e)
+
+
 ##
 ## Current Weather
 ##
 
+
 def FetchCurrentWeather():
     res = requests.get(current_weather_url)
-    if not res.status_code == 200:
-        print(res.status_code)
-        print(res.content)
+    if res.status_code != 200:
+        logging.error(res.status_code)
+        logging.error(res.content)
     assert res.status_code == 200
     return res.json()
+
 
 def ParseCurrentWeather(current_weather):
     return { # InfluxDB is picky about types
@@ -54,27 +77,20 @@ def ParseCurrentWeather(current_weather):
         'open_weather_daylight_hours': float((current_weather['sys']['sunset'] - current_weather['sys']['sunrise']) / (60 * 60)),
     }
 
+
 def PingCurrentWeather():
-    print('pinging weather api')
+    logging.info('pinging weather api')
     current_weather = FetchCurrentWeather()
     dt = datetime.fromtimestamp(current_weather['dt']).astimezone(pytz.utc)
-    print(dt)
+    logging.info(dt)
     for key, value in ParseCurrentWeather(current_weather).items():
         point = Point(key).tag("host", hostname).field("value", value).time(dt, WritePrecision.S)
         write_api.write(bucket, org, point)
 
-PingCurrentWeather()
-schedule.every(5).minutes.do(PingCurrentWeather)
 
+RunFailSafe(PingCurrentWeather)
+schedule.every(5).minutes.do(RunFailSafe, PingCurrentWeather)
 
-def job(t):
-    print("I'm working...", t)
-    response = webhook.send(text=f"Hello from pytask! {t}")
-    assert response.status_code == 200
-    assert response.body == "ok"
-    return
-
-schedule.every().day.at("14:00").do(job,'It is 14:00')
 
 while True:
     schedule.run_pending()
